958597bd4487d646ef0f20450746a6a52bf6fec6
JainMridul82 <47223276+JainMridul82@users.noreply.github.com>
2020-06-28 14:08:23 +0530

    Update BFS_Adjacency_List.c

--- BFS_Adjacency_List.c
+++ BFS_Adjacency_List.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+// The Node structure is used in the implementation of Linked Lists as part of the Adjacency List.
 struct Node{
     int data;
     struct Node *next;
 }*root=NULL;
+
+// The Queue structure is used in the BFS algorithm.
+// The Queue is implemented using Linked Lists.
 struct Queue{
     int data;
     struct Queue *next;
 }*front=NULL,*rear=NULL;
+
+// To insert integer value(data) into the Queue.
 void Enqueue(struct Queue **head,struct Queue **tail,int data){
     struct Queue *node=(struct Queue *)malloc(sizeof(struct Queue));
     node->data=data;
@@ -19,9 +26,13 @@ void Enqueue(struct Queue **head,struct Queue **tail,int data){
         *tail=node;
     }
 }
+
+// To check if the Queue is empty or not?
 int isEmptyQ(struct Queue **head,struct Queue **tail){
     return (*head==NULL && *tail==NULL)?1:0;
 }
+
+// To remove an integer data element from the exisiting Queue.
 int Dequeue(struct Queue **head,struct Queue **tail){
     int res;
     struct Queue *p=*head;
@@ -32,6 +43,8 @@ int Dequeue(struct Queue **head,struct Queue **tail){
     free(p);
     return res;
 }
+
+// To insert a node in the Linked List. Here Linked List means one of the many lists in the Adjacency List.
 void Insert(struct Node **head,int data){
     struct Node *node=(struct Node *)malloc(sizeof(struct Node));
     node->data=data;
@@ -45,22 +58,30 @@ void Insert(struct Node **head,int data){
         p->next=node;
     }
 }
+
+// To insert a particular node in a particular position in the Adjacency List
 void Insert_AL(struct Node **AL,int index,int data){
     Insert(&AL[index],data);
 }
+
+// To display the elements(data integer) in a linked list. Here linked list means one of the many lists in the Adjacency List.
 void Display(struct Node *p){
     while(p!=NULL){
         printf("%d\t",p->data);
         p=p->next;
     }
 }
+
+// To display the entire Adjacency List
 void Display_AL(struct Node **AL){
     int i;
-    for(i=1;i<6;i+=1){
+    for(i=1;i<8;i+=1){ // We are assuming there are only 1 to 7 nodes. 1 & 7 are included.
         Display(AL[i]);
         printf("\n");
     }
 }
+
+// The BFS algorithm for Adjacency List.
 void BFS(struct Node **AL,int visited[],struct Queue **head,struct Queue **tail,int u){
     int v;
     printf("%d\t",u);
@@ -79,8 +100,10 @@ void BFS(struct Node **AL,int visited[],struct Queue **head,struct Queue **tail,
         }
     }
 }
+
 int main(){
-    struct Node **AL=(struct Node **)calloc(8,sizeof(struct Node));
+    struct Node **AL=(struct Node **)calloc(8,sizeof(struct Node)); // An adjacency List is essentially an array of pointer variables.
+    //Insert_AL(name of the Adjacency List: AL,from node 1,to node 2);
     Insert_AL(AL,1,2);
     Insert_AL(AL,1,3);
     Insert_AL(AL,1,4);
@@ -99,7 +122,7 @@ int main(){
     Insert_AL(AL,5,7);
     Insert_AL(AL,6,5);
     Insert_AL(AL,7,5);
-    int visited[]={0,0,0,0,0,0,0,0};
-    BFS(AL,visited,&front,&rear,7);
+    int visited[]={0,0,0,0,0,0,0,0}; // Initially none of the nodes have been visited.
+    BFS(AL,visited,&front,&rear,7); // Run the algorithm.
     return 0;
 }
